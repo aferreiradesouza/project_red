0a09ee20497cdc744b543532203e36534fdda7eb
Arthur Ferreira <aferreiradesouza97@gmail.com>
2023-02-10 15:24:31 -0300

    ajustes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-      <div>awdawd</div>
+      <div>
+        <div className="ml-3">
+          <p className="text-sm font-medium text-gray-900">Teste</p>
+          <p className="text-sm text-gray-500">Testeeee</p>
+        </div>
+      </div>
     </>
   )
 }
